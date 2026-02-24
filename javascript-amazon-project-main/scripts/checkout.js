afcfc5d3146f7f39d2f9824598d1aae894185bc0
MohammedBenlaiter <benlaitermohammed@outlook.com>
2026-02-24 22:25:37 +0100

    add dayjs external library

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,7 +1,11 @@
 import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
-console.log(cart);
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+const today = dayjs();
+const deliveryDate = today.add(7, "days");
+console.log(deliveryDate.format("dddd, MMMM  D"));
+
 //localStorage.clear();
 let cartSummaryHTML = "";
 cart.forEach((cartItem) => {
